ef9e576084a14342ca7c9bd9cc726fb89210f45a
LupinaIgor <igorlupina222@gmail.com>
2024-09-12 12:48:32 +0300

    Authentification complete

---
+++ AuthentificationApp/AuthentificationApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AuthentificationApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
